565fc9c82b5c4eeb29a652bceae3e6daf3e75060
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-01-28 23:10:39 -0800

    updated contact component and added it to About component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,30 +6,36 @@ import Twitter from 'react-icons/lib/fa/twitter-square';
 import Linkedin from 'react-icons/lib/fa/linkedin-square';
 import Email from 'react-icons/lib/fa/envelope-square';
 
+import { Button } from 'react-bootstrap';
+
+
 class Contact extends React.Component {
   render() {
     return (
       <div className="social-div">
         <ul className="social-icons">
-          <li>
+          <li className="icons">
             <a href="https://github.com/senecasisneros">
               <h1><GitHub /></h1>
             </a>
           </li>
-          <li>
+          <li className="icons">
             <a href="https://twitter.com/sen427">
               <h1><Twitter /></h1>
             </a>
           </li>
-          <li>
+          <li >
             <a href="https://www.linkedin.com/in/seneca-sisneros-55a63a51">
               <h1><Linkedin /></h1>
             </a>
           </li>
-          <div>
-            <a href="mailto:seneca.sisneros@yahoo.com" className="morebtn">Send Email</a>
-          </div>
-        </ul>
+        <li>
+          <Button>
+            <a href="mailto:seneca.sisneros@yahoo.com" className="emailBtn">Send Email</a>
+          </Button>
+        </li>
+      </ul>
+
       </div>
     );
   }
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Contact from './Contact';
 
 class Splash extends React.Component {
   render() {
     return (
       <div className="Splash">
-        New Splash Page
+      <h1>Seneca Sisneros</h1>
+      <h3>Full-Stack Javascript Developer</h3>
+      <Contact />
       </div>
     );
   }
