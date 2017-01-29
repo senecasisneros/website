cf9547d15cd2c2356fd6bcb1e99e4e9c550f8112
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-01-29 12:12:41 -0800

    updated About

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,7 @@ class About extends React.Component {
       <div className="About">
         <div id="about">
           <Image id="profilePic" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAK8AAAAJGFlNGM3NTRhLTc3ZTYtNGMyZS1iM2VmLWRkNDk1ZjgyY2Y3MA.jpg" responsive thumbnail />
-          <p id="aboutText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.</p>
-      </div>
-      <div>
-        <Contact />
+          <p id="aboutText">My name is Seneca Sisneros and I am a full-stack javascript developer. I am a very positive minded individual who enjoys challenging myself and encouraging others to find their happiness. I have two amazing but incredibly spoiled dogs at home that remind me to stay focused on what’s really important in life and I am a firm believer that everything happens for a reason and everyone is on their own unique journey.</p>
       </div>
       </div>
     );
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -5,8 +5,8 @@ class Splash extends React.Component {
   render() {
     return (
       <div className="Splash">
-      <h1>Seneca Sisneros</h1>
-      <h3>Full-Stack Javascript Developer</h3>
+      <h1 id="name">Seneca Sisneros</h1>
+      <h3 id="title">Full-Stack Javascript Developer</h3>
       <Contact />
       </div>
     );
