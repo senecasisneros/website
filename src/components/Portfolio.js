373d160e27d2ccffb07eae034d58ceaaf1671e57
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-05-11 13:28:51 -0700

    added react-bootstrap(Grid) to Portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,93 +2,36 @@ import React from 'react';
 import animated from 'animate.css';
 import GitHub from 'react-icons/lib/fa/github-square';
 import LapTop from 'react-icons/lib/fa/laptop';
-
-
+import {Grid, Row, Col} from 'react-bootstrap';
 
 class Portfolio extends React.Component {
   render() {
     return (
-      <div>
+      <Grid className='Portfolio'>
         <h1 className="portTitle">Portfolio page</h1>
-      <div className="Portfolio">
-        <div className='portVideo'>
-          <img name="Name" src="https://media.giphy.com/media/j9xQVAZtsguUo/giphy.gif"></img>
-        <h3>The Mysterious Moon</h3>
-      <p className="textPort">This project was built with React, HTML, CSS Etc.</p>
-    <p className="textPort">this is the text about the app</p>
-  <div className="social-div">
-    <ul className="port-icons">
-      <li className="portLi">
-        <a href="https://github.com/senecasisneros">
-        <h1 className="icons">
-          <GitHub />
-      </h1>
-    </a>
-  </li>
-  <li className="portLi">
-    <a href="">
-      <h1 className="iconsPort">
-        <LapTop />
-    </h1>
-  </a>
-</li>
-</ul>
-</div>
-</div>
-<div className='portVideo'>
-  <img name="Name" src="https://media.giphy.com/media/j9xQVAZtsguUo/giphy.gif"></img>
-<h3>The Mysterious Moon</h3>
-<p className="textPort">This project was built with React, HTML, CSS Etc.</p>
-<p className="textPort">this is the text about the app</p>
-<div className="port-div">
-  <ul className="port-icons">
-    <li className="portLi">
-      <a href="https://github.com/senecasisneros">
-      <h1 className="icons">
-        <GitHub />
-    </h1>
-  </a>
-</li>
-<li className="portLi">
-  <a href="#">
-    <h1 className="iconsPort">
-      <LapTop />
-  </h1>
-</a>
-</li>
-</ul>
-</div>
-</div>
-<div className='portVideo'>
-  <img name="Name" src="https://media.giphy.com/media/j9xQVAZtsguUo/giphy.gif"></img>
-<h3>The Mysterious Moon</h3>
-<p className="textPort">This project was built with React, HTML, CSS Etc.</p>
-<p className="textPort">this is the text about the app</p>
-<div className="social-div">
-  <ul className="port-icons">
-    <li className="portLi">
-      <a href="https://github.com/senecasisneros">
-      <h1 className="icons">
-        <GitHub />
-    </h1>
-  </a>
-</li>
-<li className="portLi">
-  <a href="">
-    <h1 className="iconsPort">
-      <LapTop />
-  </h1>
-</a>
-</li>
-</ul>
-</div>
-</div>
-</div>
-</div>
-);
+        <Row className="show-grid">
+          <Col className="colPort" xs={12} sm={6} lg={4}>
+            <img className="portVid" name="Name" src="https://media.giphy.com/media/j9xQVAZtsguUo/giphy.gif"></img>
+            <h3>The Mysterious Moon</h3>
+            <p className="textPort">This project was built with React, HTML, CSS Etc.</p>
+            <p className="textPort">this is the text about the app</p>
+          </Col>
+          <Col className="colPort" xs={12} sm={6} lg={4}>
+            <img className="portVid" name="Name" src="https://media.giphy.com/media/j9xQVAZtsguUo/giphy.gif"></img>
+            <h3>The Mysterious Moon</h3>
+            <p className="textPort">This project was built with React, HTML, CSS Etc.</p>
+            <p className="textPort">this is the text about the app</p>
+          </Col>
+          <Col className="colPort" xs={12} sm={6} lg={4}>
+            <img className="portVid" name="Name" src="https://media.giphy.com/media/j9xQVAZtsguUo/giphy.gif"></img>
+            <h3>The Mysterious Moon</h3>
+            <p className="textPort">This project was built with React, HTML, CSS Etc.</p>
+            <p className="textPort">this is the text about the app</p>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
 }
-}
-
-
 
 export default Portfolio;
