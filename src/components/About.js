8d3c5187bb7665c58a4c5210737a0f1d4a3fa1e1
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-05-12 12:25:59 -0700

    fixed issue with mobile responsiveness with background image

--- src/components/About.js
+++ src/components/About.js
@@ -10,10 +10,10 @@ class About extends React.Component {
         <div id="about">
           <h1 id="aboutMe">About Me</h1>
           <h5 id="quote">The most dangerous thing you can do in life is play it safe. -Casey Neistat</h5>
-          <p id="aboutText">My name is Seneca Sisneros and I am a full-stack javascript developer. I am a very positive minded individual who enjoys challenging myself and encouraging others to find their happiness. I have two amazing but incredibly spoiled dogs at home that remind me to stay focused on what’s really important in life and I am a firm believer that everything happens for a reason and everyone is on their own unique journey.</p>
+          <p id="aboutText">My name is Seneca Sisneros and I am a full-stack javascript developer. I am a very positive minded individual who enjoys challenging myself and encouraging others to find their happiness. I have two amazing but incredibly spoiled dogs at home that remind me to stay focused on what’s really important in life and I am a firm believer that everything happens for a reason and everyone is on their own unique journey. My name is Seneca Sisneros and I am a full-stack javascript developer. I have two amazing but incredibly spoiled dogs at home that remind me to stay focused on what’s really important in life and I am a firm believer that everything happens for a reason and everyone is on their own unique journey.</p>
           <div id="over" className="animated rotateIn">
             <span id="Centerer"></span>
-            <Image className="Centered" id="sedonaFamilyPic" src="https://s3.amazonaws.com/website-seneca/family_Sedona.jpg" responsive thumbnail />
+            <Image className="Centered" id="sedonaFamilyPic" src="https://s3.amazonaws.com/website-seneca/family_Sedona.jpg" responsive thumbnail /> */}
             <Image className="Centered" id="profilePic" src="https://s3.amazonaws.com/website-seneca/senecaPic.jpg" responsive thumbnail />
             <Image className="Centered" id="pupsPic" src="https://s3.amazonaws.com/website-seneca/Pups.jpg" responsive thumbnail />
           </div>
