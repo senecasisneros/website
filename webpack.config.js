cfbf8a83b6001eeea8687c717f2ebfdbfe29eebd
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-06-02 17:01:44 -0700

    getting app ready to go live

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,42 @@
-const path = require('path');
-const webpack = require('webpack');
-
 module.exports = {
-  debug: true,
-  devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    'bootstrap-loader',
-    './src/index.jsx'
+    './src/index.js'
   ],
   output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '/'
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
   },
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        include: path.join(__dirname, 'src'),
-        loader: 'babel'
-      },
-      {test: /(\.css)$/, loaders: ['style', 'css']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-            'file?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
-    	},
-      // Bootstrap 3 jquery requirement
-      { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports?jQuery=jquery' }
-    ]
-  },
-  plugins: [
-        new webpack.ProvidePlugin({
-            "React": "react",
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
-        new webpack.OldWatchingPlugin()
-  ],
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    },
+    {test: /(\.css)$/, loaders: ['style', 'css']},
+    {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+    {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+    {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+    {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
+    {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+      ]
+    },
+    // Bootstrap 3 jquery requirement
+    { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports?jQuery=jquery' }
+  ]
+},
+resolve: {
+  extensions: ['', '.js', '.jsx']
 }
+// devServer: {
+//   historyApiFallback: true,
+//   contentBase: './'
+// }
+};
