c632d5964f77311d1ea0a3f6d6f8b23f59af6d38
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-06-12 18:56:32 -0700

    updated portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,63 +8,63 @@ import Launch from 'react-icons/lib/md/launch';
 class Portfolio extends React.Component {
   render() {
     return (
-      <div className="pppp">
-        <Grid className="Portfolio">
-          {/* <h1 className="portTitle">My Projects</h1> */}
-          <Row className="show-grid">
-            <Col className="colPort" xs={12} sm={4} lg={4}>
-              <h3 className='titlePort'>The Mysterious Moon</h3>
+    <div className="portfolioMain">
+      <Grid className="Portfolio">
+        {/* <h1 className="portTitle">My Projects</h1> */}
+        <Row className="show-grid">
+          <Col className="colPort" xs={12} sm={4} lg={4}>
+            <h3 className='titlePort'>The Mysterious Moon</h3>
             <img className="portVid" name="The Mysterious Moon" src="https://media.giphy.com/media/l4FGFIAFpQpZC4mw8/giphy.gif" />
-          <p className="textInfo">Javascript, React, HTML5, CSS3, Node.js, Babel, Bootstrap,Webpack and Eslint.</p>
-        <p className="textPort">Search by zip code and returns the current moon phase information, spirtual symbology and the next 4 moon phases.</p>
-      <div className="port-div">
-        <ul className="port-icons">
-          <li className="portLi">
-            <a href="https://github.com/senecasisneros/The-Mysterious-Moon" target="_blank">
-            <h1 className="portIcons"><GitHub /></h1>
-        </a>
-      </li>
-      <li className="portLi">
-        <a href="https://themysteriousmoon.herokuapp.com/" target="_blank"><Button className="portIcons1"><Launch />  Vist the website</Button></a>
-    </li>
-  </ul>
-</div>
-</Col>
-<Col className="colPort" xs={12} sm={4} lg={4}>
-  <h3 className='titlePort'>Museo</h3>
-<img className="portVid" name="Museo" src="https://media.giphy.com/media/l0Iy8kNUp4r9e0rBu/giphy.gif" />
-<p className="textInfo">Javascript, React, Flux, HTML5, CSS3, Node.js, Express.js, MongoDB, Babel, Webpack Eslint, Bootstrap and Materialize.</p>
-<p className="textPort">Search for an artist or song and this app with return a music video and the full lyrics of the song. </p>
-<ul className="port-icons">
-  <li className="portLi">
-    <a href="https://github.com/senecasisneros/museo" target="_blank">
-    <h1 className="portIcons"><GitHub /></h1>
-</a>
-</li>
-{/* <li className="portLi">
-  <a href="" target="_blank"><Button className="portIcons1"><Launch />  Vist the website</Button></a>
-  </li> */}
-</ul>
-</Col>
-<Col className="colPort" xs={12} sm={4} lg={4}>
-  <h3 className='titlePort'>Dash And Dine</h3>
-<img className="portVid" name="Dash And Dine" src="https://media.giphy.com/media/l0IyaY7uyHmNH2rFC/giphy.gif" />
-<p className="textInfo">React, Redux, HTML5, CSS3, Node.js, Express.js, MongoDB, Bootstrap, Babel, Webpack and Eslint</p>
-<p className="textPort">Search for a restaurant by location and this app will return a random restaurant with its rating, contact information, curent weather and the map of the location. (This was a collaboration with 3 other developers)</p>
-<ul className="port-icons">
-  <li className="portLi">
-    <a href="https://github.com/senecasisneros/dash_and_dine" target="_blank">
-    <h1 className="portIcons"><GitHub /></h1>
-</a>
-</li>
-<li className="portLi">
-  <a href="https://dashanddine-1.herokuapp.com/" target="_blank"><Button className="portIcons1"><Launch />  Vist the website</Button></a>
-</li>
-</ul>
-</Col>
-</Row>
-</Grid>
-      </div>
+            <p className="textInfo">Javascript, React, HTML5, CSS3, Node.js, Babel, Bootstrap,Webpack and Eslint.</p>
+          <p className="textPortMoon">You can search by zip code to return the current moon phase and Additional information about the moon’s location and associated spiritual meaning.</p>
+            <div className="port-div">
+              <ul className="port-icons">
+                <li className="portLi">
+                  <a href="https://github.com/senecasisneros/The-Mysterious-Moon" target="_blank">
+                    <h1 className="portIcons"><GitHub /></h1>
+                  </a>
+                </li>
+                <li className="portLi">
+                  <a href="https://themysteriousmoon.herokuapp.com/" target="_blank"><Button className="portIcons1"><Launch />  Vist the website</Button></a>
+                </li>
+              </ul>
+            </div>
+          </Col>
+          <Col className="colPort" xs={12} sm={4} lg={4}>
+            <h3 className='titlePort'>Museo</h3>
+            <img className="portVid" name="Museo" src="https://media.giphy.com/media/l0Iy8kNUp4r9e0rBu/giphy.gif" />
+            <p className="textInfo">Javascript, React, Flux, HTML5, CSS3, Node.js, Express.js, MongoDB, Babel, Webpack, Eslint, Bootstrap and Materialize.</p>
+            <p className="textPort">An application that allows you to search by artist or song name and will produce the corresponding music video and lyrics.</p>
+            <ul className="port-icons">
+              <li className="portLi">
+                <a href="https://github.com/senecasisneros/museo" target="_blank">
+                  <h1 className="portIconsMusic"><GitHub /></h1>
+                </a>
+              </li>
+              {/* <li className="portLi">
+                <a href="" target="_blank"><Button className="portIcons1"><Launch />  Vist the website</Button></a>
+              </li> */}
+            </ul>
+          </Col>
+          <Col className="colPort" xs={12} sm={4} lg={4}>
+            <h3 className='titlePort'>Dash And Dine</h3>
+            <img className="portVid" name="Dash And Dine" src="https://media.giphy.com/media/l0IyaY7uyHmNH2rFC/giphy.gif" />
+            <p className="textInfo">React, Redux, HTML5, CSS3, Node.js, Express.js, MongoDB, Bootstrap, Babel, Webpack and Eslint</p>
+            <p className="textPort">A project, in collaboration with 3 other developers, that allows you to search by location, to get a restaurant suggestion at random. It provides the contact information, rating, map, and even the current weather for that location.</p>
+            <ul className="port-icons">
+              <li className="portLi">
+                <a href="https://github.com/senecasisneros/dash_and_dine" target="_blank">
+                  <h1 className="portIcons"><GitHub /></h1>
+                </a>
+              </li>
+              <li className="portLi">
+                <a href="https://dashanddine-1.herokuapp.com/" target="_blank"><Button className="portIcons1"><Launch />  Vist the website</Button></a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Grid>
+    </div>
     );
   }
 }
