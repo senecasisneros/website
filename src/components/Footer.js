68fd4c0290ce6e2be7fc5ad85897c722c83aed70
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-06-19 16:34:50 -0700

    updated about me page and css for background image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,10 @@ import React from 'react';
 
 function Footer(/*props*/) {
   return (
-    <div id="footer">
+    <div id="footer" className="navbar-fixed-bottom">
         <p className="text-muted copyright">Copyright © Seneca Sisneros 2017</p>
     </div>
   );
 }
 
 export default Footer;
-// className=' navbar-fixed-bottom
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,73 +1,72 @@
 import React from 'react';
 // import {Image} from 'react-bootstrap';
 import animated from 'animate.css';
-import {Grid, Row, Col, Image} from 'react-bootstrap';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
 
 
 class Skills extends React.Component {
   render() {
     return (
-      <Grid className='Skills'>
-        {/* <h1 className="skillHeader">Skills</h1> */}
+    <div className="Skills">
+      <Grid>
         <Row className="show-grid row-centered">
           <Col className="iconNew" xs={12} sm={6} lg={4}>
-            <Image id="javaScript" className="animated flip" src="src/style/image/javaScript.png" responsive/>
+            <Image id="javaScript" className="animated flip" src="src/style/image/javaScript.png" responsive />
             <h5>JavaScript</h5>
           </Col>
           <Col className="iconNew" xs={12} sm={6} lg={4}>
-            <Image id="node" className="animated flip" src="src/style/image/nodejs.png" responsive/>
+            <Image id="node" className="animated flip" src="src/style/image/nodejs.png" responsive />
             <h5>Node.js</h5>
           </Col>
           <Col className="iconNew" xs={12} sm={6} lg={4}>
-            <Image id="express" className="animated flip" src="src/style/image/express.png" responsive/>
+            <Image id="express" className="animated flip" src="src/style/image/express.png" responsive />
             <h5>Express</h5>
-          </ Col>
+          </Col>
           <Col className="iconNew" xs={12} sm={6} lg={4}>
-            <Image id="mongo" className="animated flip" src="src/style/image/Mongo.png" responsive/>
+            <Image id="mongo" className="animated flip" src="src/style/image/Mongo.png" responsive />
             <h5>MongoDB</h5>
-          </ Col>
+          </Col>
         </Row>
-
-        <Row className="show-grid portRow">
+        <Row className="show-grid">
           <Col className="iconNew" xs={12} sm={6} lg={4}>
-            <Image id="html" className="animated flip" src="src/style/image/html5.svg" responsive/>
+            <Image id="html" className="animated flip" src="src/style/image/html5.svg" responsive />
             <h5>HTML5</h5>
           </Col>
           <Col className="iconNew" xs={12} sm={6} lg={4}>
-            <Image id="css" className="animated flip" src="src/style/image/css3.svg" responsive/>
+            <Image id="css" className="animated flip" src="src/style/image/css3.svg" responsive />
             <h5>CSS3</h5>
           </Col>
           <Col className="iconNew" xs={12} sm={6} lg={4}>
-            <Image id="bootstrap" className="animated flip" src="src/style/image/bootstrap.png" responsive/>
+            <Image id="bootstrap" className="animated flip" src="src/style/image/bootstrap.png" responsive />
             <h5>Bootstrap</h5>
-          </ Col>
+          </Col>
           <Col className="iconNew" xs={12} sm={6} lg={4}>
-            <Image id="react" className="animated flip" src="src/style/image/react.png" responsive/>
+            <Image id="react" className="animated flip" src="src/style/image/react.png" responsive />
             <h5>React</h5>
-          </ Col>
+          </Col>
         </Row>
-
-        <Row className="show-grid portRow">
+        <Row className="show-grid">
           <Col className="iconNew" xs={12} sm={6} lg={4}>
-            <Image id="flux" className="animated flip" src="src/style/image/flux.png" responsive/>
+            <Image id="flux" className="animated flip" src="src/style/image/flux.png" responsive />
             <h5>Flux</h5>
           </Col>
           <Col className="iconNew" xs={12} sm={6} lg={4}>
-            <Image id="redux" className="animated flip" src="src/style/image/redux.png" responsive/>
+            <Image id="redux" className="animated flip" src="src/style/image/redux.png" responsive />
             <h5>Redux</h5>
           </Col>
           <Col className="iconNew" xs={12} sm={6} lg={4}>
-            <Image id="mysql" className="animated flip" src="src/style/image/sql.png" responsive/>
+            <Image id="mysql" className="animated flip" src="src/style/image/sql.png" responsive />
             <h5>SQL</h5>
-          </ Col>
+          </Col>
           <Col className="iconNew" xs={12} sm={6} lg={4}>
-            <Image id="jQuery" className="animated flip" src="src/style/image/jquery-icon.png" responsive/>
+            <Image id="jQuery" className="animated flip" src="src/style/image/jquery-icon.png" responsive />
             <h5>jQuery</h5>
-          </ Col>
+          </Col>
         </Row>
       </Grid>
+    </div>
     );
   }
 }
 
-export default Skills;
+        export default Skills;
