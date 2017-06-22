13613ddd3cfdb5f058296ade5fc02c6b9f744c1f
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-06-21 23:41:01 -0700

    changed name to linkedin image

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,26 +5,26 @@ class Contact extends React.Component {
   render() {
     return (
       <div className="social-div">
-          <div className="socialLi">
-            <a href="https://github.com/senecasisneros" target="_blank">
-            <img id='github' className="icons" src='../src/style/image/GitHub.png' />
-            </a>
-          </div>
-          <div className="socialLi">
-            <a href="https://twitter.com/sen427" target="_blank">
+        <div className="socialLi">
+          <a href="https://github.com/senecasisneros" target="_blank">
+          <img id='github' className="icons" src='../src/style/image/GitHub.png' />
+          </a>
+        </div>
+        <div className="socialLi">
+          <a href="https://twitter.com/sen427" target="_blank">
             <img id='twiter' className="icons" src='../src/style/image/Twitter_Social.png' />
-            </a>
-          </div>
-          <div className="socialLi">
-            <a href="https://www.linkedin.com/in/seneca-sisneros-55a63a51" target="_blank">
-              <img id="linkedin" className="icons" src='../src/style/image/Linkedin.png' />
-            </a>
-          </div>
-          <div className='socialLi'>
-            <a href="mailto:seneca.sisneros@yahoo.com" target="_blank">
-              <img id="email" className="icons" src='../src/style/image/Yahoo.png' />
-            </a>
-          </div>
+          </a>
+        </div>
+        <div className="socialLi">
+          <a href="https://www.linkedin.com/in/seneca-sisneros-55a63a51" target="_blank">
+            <img id="linkedin" className="icons" src='../src/style/image/linkedin.png' />
+          </a>
+        </div>
+        <div className='socialLi'>
+          <a href="mailto:seneca.sisneros@yahoo.com" target="_blank">
+            <img id="email" className="icons" src='../src/style/image/Yahoo.png' />
+          </a>
+        </div>
       </div>
     );
   }
